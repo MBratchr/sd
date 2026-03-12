978ef6d064c55d3c1da476aed55e6a59c8afe9e2
MBratcher <mbratcher2021@my.fit.edu>
2026-03-12 15:11:14 -0400

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FileViewer } from './file-viewer/file-viewer';
 import { InspectionManager } from './inspection-manager/inspection-manager';
 import { RegisterTrace } from './register-trace/register-trace';
-import type { TraceFrame } from './register-trace/register-trace.models';
+import { TraceResultService } from './services/trace-result.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,9 @@ import type { TraceFrame } from './register-trace/register-trace.models';
   styleUrl: './app.scss',
 })
 export class App {
+  private traceResult = inject(TraceResultService);
+  readonly backendResult = this.traceResult.result;
+
   isDarkMode =
     typeof window !== 'undefined' &&
     window.matchMedia &&
@@ -20,31 +23,4 @@ export class App {
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
   }
-
-  // Mock trace based on your NASM "Hello, World!" program
-  mockTrace: TraceFrame[] = [
-    // mov eax, 4
-    { line: 12, regs: { EAX: 4, EBX: 0, ECX: 0, EDX: 0 }, flags: { ZF: 0, CF: 0 } },
-
-    // mov ebx, 1
-    { line: 13, regs: { EAX: 4, EBX: 1, ECX: 0, EDX: 0 }, flags: { ZF: 0, CF: 0 } },
-
-    // mov ecx, msg
-    { line: 14, regs: { EAX: 4, EBX: 1, ECX: '0x0804A000', EDX: 0 }, flags: { ZF: 0, CF: 0 } },
-
-    // mov edx, len  (len = 14)
-    { line: 15, regs: { EAX: 4, EBX: 1, ECX: '0x0804A000', EDX: 14 }, flags: { ZF: 0, CF: 0 } },
-
-    // int 0x80  (sys_write)
-    { line: 16, regs: { EAX: 4, EBX: 1, ECX: '0x0804A000', EDX: 14 }, flags: { ZF: 0, CF: 0 } },
-
-    // mov eax, 1
-    { line: 18, regs: { EAX: 1, EBX: 1, ECX: '0x0804A000', EDX: 14 }, flags: { ZF: 0, CF: 0 } },
-
-    // xor ebx, ebx  (EBX -> 0, ZF -> 1)
-    { line: 19, regs: { EAX: 1, EBX: 0, ECX: '0x0804A000', EDX: 14 }, flags: { ZF: 1, CF: 0 } },
-
-    // int 0x80 (sys_exit)
-    { line: 20, regs: { EAX: 1, EBX: 0, ECX: '0x0804A000', EDX: 14 }, flags: { ZF: 1, CF: 0 } },
-  ];
 }
